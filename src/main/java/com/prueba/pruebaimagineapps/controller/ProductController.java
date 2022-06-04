c78c5af6b7f318aeba9e88999512dbd7596ea5ed
Fermin Colorado <fermin.ciro@esumer.edu.co>
2022-06-04 15:19:06 -0500

    Se crea método para guardar un prodcuto vista /create y LOGGER para la clase ProductController

--- src/main/java/com/prueba/pruebaimagineapps/controller/ProductController.java
+++ src/main/java/com/prueba/pruebaimagineapps/controller/ProductController.java
@@ -1,12 +1,23 @@
 package com.prueba.pruebaimagineapps.controller;
 
+import com.prueba.pruebaimagineapps.model.Product;
+import com.prueba.pruebaimagineapps.model.User;
+import com.prueba.pruebaimagineapps.service.ProductService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductController {
+
+    private final Logger LOOGGER = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping ("")
     public String show() {
@@ -17,4 +28,15 @@ public class ProductController {
     public String create(){
         return "productos/create";
     }
+
+    @PostMapping ("/save")
+    public String save(Product product){
+        User user = new User(1,"Fermin Colorado", "fermincolorado","fermin.ciro@rsumer.edu.co", "Cll 25 a 38", "3023290274", "ADMIN", "12345678");
+        product.setUser(user);
+        productService.save(product);
+        LOOGGER.info("Este es el objeto del prodcuto {}", product);
+        return "redirect:/productos";
+
+    }
+
 }
