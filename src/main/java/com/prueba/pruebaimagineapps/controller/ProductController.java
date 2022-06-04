396f7167216c6d07d8622e16d0abb99bed3517ee
Fermin Colorado <fermin.ciro@esumer.edu.co>
2022-06-04 18:09:03 -0500

    Se crea método para editar la información de un producto

--- src/main/java/com/prueba/pruebaimagineapps/controller/ProductController.java
+++ src/main/java/com/prueba/pruebaimagineapps/controller/ProductController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
-@RequestMapping("/productos")
+@RequestMapping("/products")
 public class ProductController {
 
     private final Logger LOOGGER = LoggerFactory.getLogger(ProductController.class);
@@ -20,25 +23,42 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping ("")
+    @GetMapping("")
     public String show(Model model) {
         model.addAttribute("products", productService.findAll());
-        return "productos/show";
+        return "products/show";
     }
 
-    @GetMapping ("/create")
-    public String create(){
-        return "productos/create";
+    @GetMapping("/create")
+    public String create() {
+        return "products/create";
     }
 
-    @PostMapping ("/save")
-    public String save(Product product){
-        User user = new User(1,"Fermin Colorado", "fermincolorado","fermin.ciro@rsumer.edu.co", "Cll 25 a 38", "3023290274", "ADMIN", "12345678");
+    @PostMapping("/save")
+    public String save(Product product) {
+        User user = new User(1, "Fermin Colorado", "fermincolorado", "fermin.ciro@rsumer.edu.co", "Cll 25 a 38", "3023290274", "ADMIN", "12345678");
         product.setUser(user);
         productService.save(product);
         LOOGGER.info("Este es el objeto del prodcuto {}", product);
-        return "redirect:/productos";
-
+        return "redirect:/products";
     }
+
+    @GetMapping ("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Product product = new Product();
+        Optional<Product> optionalProduct=productService.get(id);
+        product= optionalProduct.get();
+
+        LOOGGER.info("Producto buscado: {}", product);
+        model.addAttribute("product", product);
+        return "products/edit";
+    }
+
+    @PostMapping ("/update")
+    public String update(Product product){
+        productService.update(product);
+        return "redirect:/products";
+    }
+
 
 }
